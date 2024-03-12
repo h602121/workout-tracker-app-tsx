effc997e164253f99a18ff946d09461659faf34b
Tor Magne Solheimsnes <602116@stud.hvl.no>
2024-03-12 14:22:17 +0100

    Installed and set up Expo Router

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+
+export default function Page() {
+
+  return <View> 
+     <Text>Open up App.js to start working on your app!</Text>
+     <Link href="/about">About</Link>
+     </View>
+ 
+}  
