83ee29ad4c0a7799d2f886e25830dab2da3a8827
h602121 <74725234+h602121@users.noreply.github.com>
2024-03-18 15:38:18 +0100

    all is now good and well

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,6 @@ import { StyleSheet, View, Text } from 'react-native';
 import { router, useNavigation } from 'expo-router';
  
  export default function Main(){
-     router.back()
+     
     return <Text>TEST</Text>;
 }
