3fce131c9f9384d7303a5b27194d00d121f255ae
h602121 <74725234+h602121@users.noreply.github.com>
2024-03-19 15:02:04 +0100

    Handled keyboard exit for iphone

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import TextInput from '../components/TextInput';
-import LoginButton from '../components/LoginButton';
-import { router, useNavigation } from 'expo-router';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import TextInput from "../components/TextInput";
+import LoginButton from "../components/LoginButton";
+import { router, useNavigation } from "expo-router";
 
 const LoginScreen: React.FC = () => {
+  const [email, setEmail] = useState("");
+  function changeEmail(email: string) {
+    setEmail(email);
+  }
+
   return (
-    <View style={styles.container}>
-      <TextInput placeholder="Email" keyboardType="email-address" autoCapitalize="none" autoComplete="email" textContentType="emailAddress" />
-      <TextInput placeholder="Password" secureTextEntry autoComplete="password" textContentType="password" />
-      <LoginButton onPress={() => router.push('/main')}>Login</LoginButton>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <Text>{email}</Text>
+        <TextInput
+          value={email}
+          onChangeText={changeEmail}
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoComplete="email"
+          textContentType="emailAddress"
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry
+          autoComplete="password"
+          textContentType="password"
+        />
+        <LoginButton onPress={() => router.push("/main")}>Login</LoginButton>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
     paddingHorizontal: 20,
   },
 });
